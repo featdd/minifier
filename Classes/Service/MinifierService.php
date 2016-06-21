3a6cfca27688bf6de397325f071700e85a7cf5f7
Daniel Dorndorf <dorndorf@featdd.de>
2016-06-21 13:43:46 +0200

    [TASK] Add html minification
    thanks to https://gist.github.com/tovic/d7b310dea3b33e4732c0

--- Classes/Service/MinifierService.php
+++ Classes/Service/MinifierService.php
@@ -40,9 +40,9 @@ class MinifierService implements SingletonInterface
     const ASSET_PREFIX = 'minifier-';
 
     /**
-     * @param string $page
+     * @param string $html
      */
-    public function compressPage(&$page)
+    public function compressPage(&$html)
     {
         $callback = function ($match) {
             $filePath = $match[2];
@@ -52,17 +52,19 @@ class MinifierService implements SingletonInterface
             return $match[1] . $filePath . $match[3];
         };
 
-        $page = preg_replace_callback(
+        $html = preg_replace_callback(
             '#(<link\s+(?:[^>]*?\s+)?href=")([^"]*)(")#i',
             $callback,
-            $page
+            $html
         );
 
-        $page = preg_replace_callback(
+        $html = preg_replace_callback(
             '#(<script\s+(?:[^>]*?\s+)?src=")([^"]*)(")#i',
             $callback,
-            $page
+            $html
         );
+
+        $this->minifyHTML($html);
     }
 
     /**
@@ -107,5 +109,81 @@ class MinifierService implements SingletonInterface
         }
 
         return $minifiedFilePath;
+    }
+
+    /**
+     * @param string $html
+     */
+    protected function minifyHTML(&$html)
+    {
+        // Remove extra white-space(s) between HTML attribute(s)
+        $html = preg_replace_callback(
+            '#<([^\/\s<>!]+)(?:\s+([^<>]*?)\s*|\s*)(\/?)>#s',
+            function ($matches) {
+                return '<' . $matches[1] . preg_replace(
+                    '#([^\s=]+)(\=([\'"]?)(.*?)\3)?(\s+|$)#s',
+                    ' $1$2',
+                    $matches[2]
+                ) . $matches[3] . '>';
+            },
+            str_replace("\r", "", $html)
+        );
+
+        // Minify inline CSS declaration(s)
+        if (false !== strpos($html, ' style=')) {
+            $minifier = new Minify\CSS();
+
+            $html = preg_replace_callback('#<([^<]+?)\s+style=([\'"])(.*?)\2(?=[\/\s>])#s',
+                function ($matches) use ($minifier) {
+                    $minifier->add($matches[3]);
+                    return '<' . $matches[1] . ' style=' . $matches[2] . $minifier->minify() . $matches[2];
+                },
+                $html
+            );
+        }
+
+        $html = preg_replace(
+            array(
+                // t = text
+                // o = tag open
+                // c = tag close
+                // Keep important white-space(s) after self-closing HTML tag(s)
+                '#<(img|input)(>| .*?>)#s',
+                // Remove a line break and two or more white-space(s) between tag(s)
+                '#(<!--.*?-->)|(>)(?:\n*|\s{2,})(<)|^\s*|\s*$#s',
+                // t+c || o+t
+                '#(<!--.*?-->)|(?<!\>)\s+(<\/.*?>)|(<[^\/]*?>)\s+(?!\<)#s',
+                // o+o || c+c
+                '#(<!--.*?-->)|(<[^\/]*?>)\s+(<[^\/]*?>)|(<\/.*?>)\s+(<\/.*?>)#s',
+                // c+t || t+o || o+t -- separated by long white-space(s)
+                '#(<!--.*?-->)|(<\/.*?>)\s+(\s)(?!\<)|(?<!\>)\s+(\s)(<[^\/]*?\/?>)|(<[^\/]*?\/?>)\s+(\s)(?!\<)#s',
+                // empty tag
+                '#(<!--.*?-->)|(<[^\/]*?>)\s+(<\/.*?>)#s',
+                // reset previous fix
+                '#<(img|input)(>| .*?>)<\/\1\x1A>#s',
+                // clean up ...
+                '#(&nbsp;)&nbsp;(?![<\s])#',
+                // Force line-break with `&#10;` or `&#xa;`
+                '#&\#(?:10|xa);#',
+                // Force white-space with `&#32;` or `&#x20;`
+                '#&\#(?:32|x20);#',
+                // Remove HTML comment(s) except IE comment(s)
+                '#\s*<!--(?!\[if\s).*?-->\s*|(?<!\>)\n+(?=\<[^!])#s',
+            ),
+            array(
+                "<$1$2</$1\x1A>",
+                '$1$2$3',
+                '$1$2$3',
+                '$1$2$3$4$5',
+                '$1$2$3$4$5$6$7',
+                '$1$2$3',
+                '<$1$2',
+                '$1 ',
+                "\n",
+                ' ',
+                "",
+            ),
+            $html
+        );
     }
 }

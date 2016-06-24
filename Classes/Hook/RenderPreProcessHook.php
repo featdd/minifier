3277985dace291675f25cd81f3abb2e491aefdd9
Daniel Dorndorf <dorndorf@featdd.de>
2016-06-24 15:00:54 +0200

    [TASK] Implement a concatination of all files while respecting CDN minification option

--- Classes/Hook/RenderPreProcessHook.php
+++ Classes/Hook/RenderPreProcessHook.php
@@ -27,8 +27,6 @@ namespace Featdd\Minifier\Hook;
 
 use Featdd\Minifier\Service\MinifierService;
 use TYPO3\CMS\Core\Page\PageRenderer;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
 
 /**
  *
@@ -65,22 +63,32 @@ class RenderPreProcessHook
     public function main(array &$params, PageRenderer $pageRenderer)
     {
         if ('FE' === TYPO3_MODE) {
-            $this->replaceAssets($params['cssFiles'], self::KEY_PATH);
-            $this->replaceAssets($params['jsFiles'], self::KEY_PATH);
-            $this->replaceAssets($params['jsLibs'], self::KEY_ORIGINAL);
+            if (true === (boolean) $this->extConf['concatenate']) {
+                $this->replaceAssetsConcatinated($params['cssFiles'], 'css', self::KEY_PATH);
+                $this->replaceAssetsConcatinated($params['jsFiles'], 'js', PageRenderer::PART_FOOTER, self::KEY_PATH);
+                $this->replaceAssetsConcatinated($params['jsLibs'], 'js', PageRenderer::PART_HEADER,
+                    self::KEY_ORIGINAL);
 
-            if (true === is_array($params['cssLibs'])) {
-                $this->replaceAssets($params['cssLibs'], self::KEY_ORIGINAL);
+                if (true === is_array($params['cssLibs'])) {
+                    $this->replaceAssetsConcatinated($params['cssLibs'], 'css', self::KEY_ORIGINAL);
+                }
+            } else {
+                $this->replaceAssets($params['cssFiles'], self::KEY_PATH);
+                $this->replaceAssets($params['jsFiles'], self::KEY_PATH);
+                $this->replaceAssets($params['jsLibs'], self::KEY_ORIGINAL);
+
+                if (true === is_array($params['cssLibs'])) {
+                    $this->replaceAssets($params['cssLibs'], self::KEY_ORIGINAL);
+                }
             }
         }
     }
 
     /**
-     * @param array   $files
-     * @param string  $keyToUse
-     * @param boolean $concatinate
+     * @param array  $files
+     * @param string $keyToUse
      */
-    protected function replaceAssets(array &$files, $keyToUse = self::KEY_ORIGINAL, $concatinate = false)
+    protected function replaceAssets(array &$files, $keyToUse = self::KEY_ORIGINAL)
     {
         foreach ($files as $key => $file) {
             if (
@@ -108,6 +116,78 @@ class RenderPreProcessHook
                 $files[$minifiedFilePath] = $file;
                 unset($files[$file['file']]);
             }
+        }
+    }
+
+    /**
+     * @param array   $files
+     * @param string  $fileExtension
+     * @param integer $section
+     * @param string  $keyToUse
+     */
+    protected function replaceAssetsConcatinated(array &$files, $fileExtension, $section = PageRenderer::PART_HEADER, $keyToUse = self::KEY_ORIGINAL)
+    {
+        $concatinations = array();
+
+        $minifiedFilePath = 'typo3temp/' . self::ASSET_PREFIX . md5(json_encode($files)) . '.' . $fileExtension;
+
+        foreach ($files as $key => $file) {
+            if (
+                false === (boolean) $this->extConf['minifyCDN'] &&
+                (
+                    null !== parse_url($file['file'], PHP_URL_SCHEME) ||
+                    '//' === substr($file['file'], 0, 2)
+                )
+            ) {
+                continue;
+            }
+
+            if (false === file_exists(PATH_site . $minifiedFilePath)) {
+                $concatinations[] = MinifierService::minifyFile($file['file']);
+            }
+
+            unset($files[$key]);
+        }
+
+        if (false === file_exists(PATH_site . $minifiedFilePath)) {
+            if (0 === count($concatinations)) {
+                return;
+            }
+
+            file_put_contents(PATH_site . $minifiedFilePath, implode(PHP_EOL, $concatinations));
+        }
+
+        if ($keyToUse === self::KEY_ORIGINAL) {
+            $key = 'minifier-concatinated';
+        } else {
+            $key = $minifiedFilePath;
+        }
+
+        if ('css' === $fileExtension) {
+            $files[$key] = array(
+                'file' => $minifiedFilePath,
+                'rel' => 'stylesheet',
+                'media' => 'all',
+                'title' => '',
+                'compress' => true,
+                'forceOnTop' => false,
+                'allWrap' => '',
+                'excludeFromConcatenation' => false,
+                'splitChar' => '|',
+            );
+        } else {
+            $files[$key] = array(
+                'file' => $minifiedFilePath,
+                'type' => 'text/javascript',
+                'section' => $section,
+                'compress' => true,
+                'forceOnTop' => false,
+                'allWrap' => '',
+                'excludeFromConcatenation' => false,
+                'splitChar' => '|',
+                'async' => false,
+                'integrity' => '',
+            );
         }
     }
 }
